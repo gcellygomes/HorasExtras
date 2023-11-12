62bbfb312bbac23fdd004b9ddb57d590843cf871
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-11-12 14:05:31 -0300

    VersaoOficial

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿//Exercicio HorasExtras GicellyGomes
 ﻿Console.WriteLine("*** Horas Extras ***\n");
 
-Console.WriteLine($"Salario-hora de {12:C}, {200} horas, sendo {0,2} horas-extra, salário = {Salario(12, 300, 0):C}");
-Console.WriteLine($"Salario-hora de {12:C}, {200} horas, sendo {20} horas-extra, salário = {Salario(12, 300, 20):C}");
-Console.WriteLine($"Salario-hora de {24:C}, {200} horas, sendo {0,2} horas-extra, salário = {Salario(24, 300, 0):C}");
-Console.WriteLine($"Salario-hora de {24:C}, {200} horas, sendo {20} horas-extra, salário = {Salario(24, 300, 20):C}");
+Console.WriteLine($"Salario-hora de {12:C}, {200} horas, sendo {0,2} horas-extra, salário = {Salario(12, 200, 0):C}");
+Console.WriteLine($"Salario-hora de {12:C}, {200} horas, sendo {20} horas-extra, salário = {Salario(12, 200, 20):C}");
+Console.WriteLine($"Salario-hora de {24:C}, {200} horas, sendo {0,2} horas-extra, salário = {Salario(24, 200, 0):C}");
+Console.WriteLine($"Salario-hora de {24:C}, {200} horas, sendo {20} horas-extra, salário = {Salario(24, 200, 20):C}");
 
 double Salario(double SalarioHora, double HorasTotais, double HorasExtras)
 {
